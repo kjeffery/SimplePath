5b8a5b05f394335b8d706ed5dc13ddf1c594ef99
Keith Jeffery <krjeffery@gmail.com>
2022-09-13 14:09:35 -0700

    Intial commit

---
+++ main.cpp
@@ -0,0 +1,31 @@
+///@author Keith Jeffery
+
+#include "base/Util.h"
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+namespace fs =  std::filesystem;
+
+void parse_file(std::istream&);
+
+int main(const int argc, const char* const argv[])
+{
+    using namespace std::literals;
+
+    if (argc == 2) {
+        if (std::string_view file_name{argv[1]}; file_name == "-"sv) {
+            parse_file(std::cin);
+        } else {
+            fs::path file_path{file_name};
+            std::ifstream ins(file_path);
+            if (!ins) {
+                std::cerr << "Unable to open file " << file_name << '\n';
+                return EXIT_FAILURE;
+            }
+            parse_file(ins);
+        }
+    }
+}

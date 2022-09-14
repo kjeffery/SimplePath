c1688ff0da200137fae1aafa15f70df8a79131de
Keith Jeffery <krjeffery@gmail.com>
2022-09-13 21:35:18 -0700

    Cleanup

--- math/Vector3.h
+++ math/Vector3.h
@@ -26,7 +26,9 @@ struct alignas(16) Vector3
 
         struct
         {
-            float x, y, z;
+            float x;
+            float y;
+            float z;
         };
     };
 
@@ -43,7 +45,7 @@ struct alignas(16) Vector3
     {
     }
 
-    Vector3(const __m128 a) noexcept
+    explicit Vector3(const __m128 a) noexcept
     : m128(a)
     {
     }
@@ -69,7 +71,7 @@ struct alignas(16) Vector3
     {
     }
 
-    explicit Vector3(const __m128i a)
+    explicit Vector3(const __m128i a) noexcept
     : m128(_mm_cvtepi32_ps(a))
     {
     }
@@ -78,7 +80,7 @@ struct alignas(16) Vector3
     /// Loads and Stores
     ////////////////////////////////////////////////////////////////////////////////
 
-    static Vector3 load(const void* const a)
+    static Vector3 load(const void* const a) noexcept
     {
 #if defined(__aarch64__)
         __m128 t = _mm_load_ps((float*)a);
@@ -89,12 +91,12 @@ struct alignas(16) Vector3
 #endif
     }
 
-    static Vector3 loadu(const void* const a)
+    static Vector3 loadu(const void* const a) noexcept
     {
         return Vector3(_mm_loadu_ps((float*)a));
     }
 
-    static void storeu(void* ptr, const Vector3& v)
+    static void storeu(void* ptr, const Vector3& v) noexcept
     {
         _mm_storeu_ps((float*)ptr, v.m128);
     }
@@ -127,7 +129,7 @@ struct alignas(16) Vector3
     /// Array Access
     ////////////////////////////////////////////////////////////////////////////////
 
-    const float& operator[](const size_t index) const
+    const float& operator[](const size_t index) const noexcept
     {
         assert(index < 3);
         switch (index) {
@@ -142,7 +144,7 @@ struct alignas(16) Vector3
         }
     }
 
-    float& operator[](const size_t index)
+    float& operator[](const size_t index) noexcept
     {
         assert(index < 3);
         switch (index) {
@@ -164,53 +166,53 @@ struct alignas(16) Vector3
 
 #if defined(__aarch64__)
 template <int i0, int i1, int i2, int i3>
-inline Vector3 shuffle(const Vector3& v)
+inline Vector3 shuffle(const Vector3& v) noexcept
 {
     return vreinterpretq_f32_u8(vqtbl1q_u8((uint8x16_t)v.m128, _MN_SHUFFLE(i0, i1, i2, i3)));
 }
 
 template <int i0, int i1, int i2, int i3>
-inline Vector3 shuffle(const Vector3& a, const Vector3& b)
+inline Vector3 shuffle(const Vector3& a, const Vector3& b) noexcept
 {
     return vreinterpretq_f32_u8(
         vqtbl2q_u8((uint8x16x2_t){ (uint8x16_t)a.m128, (uint8x16_t)b.m128 }, _MF_SHUFFLE(i0, i1, i2, i3)));
 }
 #else
 template <int i0, int i1, int i2, int i3>
-inline Vector3 shuffle(const Vector3& v)
+inline Vector3 shuffle(const Vector3& v) noexcept
 {
-    return _mm_castsi128_ps(_mm_shuffle_epi32(_mm_castps_si128(v.m128), _MM_SHUFFLE(i3, i2, i1, i0)));
+    return Vector3{ _mm_castsi128_ps(_mm_shuffle_epi32(_mm_castps_si128(v.m128), _MM_SHUFFLE(i3, i2, i1, i0))) };
 }
 
 template <int i0, int i1, int i2, int i3>
-inline Vector3 shuffle(const Vector3& a, const Vector3& b)
+inline Vector3 shuffle(const Vector3& a, const Vector3& b) noexcept
 {
-    return _mm_shuffle_ps(a, b, _MM_SHUFFLE(i3, i2, i1, i0));
+    return Vector3{ _mm_shuffle_ps(a, b, _MM_SHUFFLE(i3, i2, i1, i0)) };
 }
 #endif
 
 #if defined(__SSE3__) && !defined(__aarch64__)
 template <>
-inline Vector3 shuffle<0, 0, 2, 2>(const Vector3& v)
+inline Vector3 shuffle<0, 0, 2, 2>(const Vector3& v) noexcept
 {
-    return _mm_moveldup_ps(v.m128);
+    return Vector3{ _mm_moveldup_ps(v.m128) };
 }
 
 template <>
-inline Vector3 shuffle<1, 1, 3, 3>(const Vector3& v)
+inline Vector3 shuffle<1, 1, 3, 3>(const Vector3& v) noexcept
 {
-    return _mm_movehdup_ps(v.m128);
+    return Vector3{ _mm_movehdup_ps(v.m128) };
 }
 
 template <>
-inline Vector3 shuffle<0, 1, 0, 1>(const Vector3& v)
+inline Vector3 shuffle<0, 1, 0, 1>(const Vector3& v) noexcept
 {
-    return _mm_castpd_ps(_mm_movedup_pd(_mm_castps_pd(v.m128)));
+    return Vector3{ _mm_castpd_ps(_mm_movedup_pd(_mm_castps_pd(v.m128))) };
 }
 #endif
 
 template <int i>
-inline Vector3 shuffle(const Vector3& v)
+inline Vector3 shuffle(const Vector3& v) noexcept
 {
     return shuffle<i, i, i, i>(v);
 }
@@ -219,42 +221,42 @@ inline Vector3 shuffle(const Vector3& v)
 /// Unary Operators
 ////////////////////////////////////////////////////////////////////////////////
 
-inline Vector3 operator+(const Vector3& a)
+inline Vector3 operator+(const Vector3& a) noexcept
 {
     return a;
 }
 
-inline Vector3 operator-(const Vector3& a)
+inline Vector3 operator-(const Vector3& a) noexcept
 {
 #if defined(__aarch64__)
     return vnegq_f32(a.m128);
 #else
     const __m128 mask = _mm_castsi128_ps(_mm_set1_epi32(0x80000000));
-    return _mm_xor_ps(a.m128, mask);
+    return Vector3{ _mm_xor_ps(a.m128, mask) };
 #endif
 }
 
-inline Vector3 abs(const Vector3& a)
+inline Vector3 abs(const Vector3& a) noexcept
 {
 #if defined(__aarch64__)
     return _mm_abs_ps(a.m128);
 #else
     const __m128 mask = _mm_castsi128_ps(_mm_set1_epi32(0x7fffffff));
-    return _mm_and_ps(a.m128, mask);
+    return Vector3{ _mm_and_ps(a.m128, mask) };
 #endif
 }
 
-inline Vector3 sqrt(const Vector3& a)
+inline Vector3 sqrt(const Vector3& a) noexcept
 {
-    return _mm_sqrt_ps(a.m128);
+    return Vector3{ _mm_sqrt_ps(a.m128) };
 }
 
-inline Vector3 sqr(const Vector3& a)
+inline Vector3 sqr(const Vector3& a) noexcept
 {
-    return _mm_mul_ps(a.m128, a.m128);
+    return Vector3{ _mm_mul_ps(a.m128, a.m128) };
 }
 
-inline Vector3 rsqrt(const Vector3& a)
+inline Vector3 rsqrt(const Vector3& a) noexcept
 {
 #if defined(__aarch64__)
     __m128 r = _mm_rsqrt_ps(a.m128);
@@ -268,8 +270,8 @@ inline Vector3 rsqrt(const Vector3& a)
 #else
     __m128 r = _mm_rsqrt_ps(a.m128);
 #endif
-    return _mm_add_ps(_mm_mul_ps(_mm_set1_ps(1.5f), r),
-                      _mm_mul_ps(_mm_mul_ps(_mm_mul_ps(a.m128, _mm_set1_ps(-0.5f)), r), _mm_mul_ps(r, r)));
+    return Vector3{ _mm_add_ps(_mm_mul_ps(_mm_set1_ps(1.5f), r),
+                               _mm_mul_ps(_mm_mul_ps(_mm_mul_ps(a.m128, _mm_set1_ps(-0.5f)), r), _mm_mul_ps(r, r))) };
 #endif
 }
 
@@ -277,54 +279,54 @@ inline Vector3 rsqrt(const Vector3& a)
 /// Binary Operators
 ////////////////////////////////////////////////////////////////////////////////
 
-inline Vector3 operator+(const Vector3& a, const Vector3& b)
+inline Vector3 operator+(const Vector3& a, const Vector3& b) noexcept
 {
-    return _mm_add_ps(a.m128, b.m128);
+    return Vector3{_mm_add_ps(a.m128, b.m128)};
 }
 
-inline Vector3 operator-(const Vector3& a, const Vector3& b)
+inline Vector3 operator-(const Vector3& a, const Vector3& b) noexcept
 {
-    return _mm_sub_ps(a.m128, b.m128);
+    return Vector3{_mm_sub_ps(a.m128, b.m128)};
 }
 
-inline Vector3 operator*(const Vector3& a, const Vector3& b)
+inline Vector3 operator*(const Vector3& a, const Vector3& b) noexcept
 {
-    return _mm_mul_ps(a.m128, b.m128);
+    return Vector3{_mm_mul_ps(a.m128, b.m128)};
 }
 
-inline Vector3 operator*(const Vector3& a, const float b)
+inline Vector3 operator*(const Vector3& a, const float b) noexcept
 {
     return a * Vector3(b);
 }
 
-inline Vector3 operator*(const float a, const Vector3& b)
+inline Vector3 operator*(const float a, const Vector3& b) noexcept
 {
     return Vector3(a) * b;
 }
 
-inline Vector3 operator/(const Vector3& a, const Vector3& b)
+inline Vector3 operator/(const Vector3& a, const Vector3& b) noexcept
 {
-    return _mm_div_ps(a.m128, b.m128);
+    return Vector3{_mm_div_ps(a.m128, b.m128)};
 }
 
-inline Vector3 operator/(const Vector3& a, const float b)
+inline Vector3 operator/(const Vector3& a, const float b) noexcept
 {
-    return _mm_div_ps(a.m128, _mm_set1_ps(b));
+    return Vector3{_mm_div_ps(a.m128, _mm_set1_ps(b))};
 }
 
-inline Vector3 operator/(const float a, const Vector3& b)
+inline Vector3 operator/(const float a, const Vector3& b) noexcept
 {
-    return _mm_div_ps(_mm_set1_ps(a), b.m128);
+    return Vector3{_mm_div_ps(_mm_set1_ps(a), b.m128)};
 }
 
-inline Vector3 min(const Vector3& a, const Vector3& b)
+inline Vector3 min(const Vector3& a, const Vector3& b) noexcept
 {
-    return _mm_min_ps(a.m128, b.m128);
+    return Vector3{_mm_min_ps(a.m128, b.m128)};
 }
 
-inline Vector3 max(const Vector3& a, const Vector3& b)
+inline Vector3 max(const Vector3& a, const Vector3& b) noexcept
 {
-    return _mm_max_ps(a.m128, b.m128);
+    return Vector3{_mm_max_ps(a.m128, b.m128)};
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -332,70 +334,70 @@ inline Vector3 max(const Vector3& a, const Vector3& b)
 ////////////////////////////////////////////////////////////////////////////////
 
 #if defined(__AVX2__) || defined(__ARM_NEON)
-inline Vector3 madd(const Vector3& a, const Vector3& b, const Vector3& c)
+inline Vector3 madd(const Vector3& a, const Vector3& b, const Vector3& c) noexcept
 {
-    return _mm_fmadd_ps(a.m128, b.m128, c.m128);
+    return Vector3{_mm_fmadd_ps(a.m128, b.m128, c.m128)};
 }
 
-inline Vector3 msub(const Vector3& a, const Vector3& b, const Vector3& c)
+inline Vector3 msub(const Vector3& a, const Vector3& b, const Vector3& c) noexcept
 {
-    return _mm_fmsub_ps(a.m128, b.m128, c.m128);
+    return Vector3{_mm_fmsub_ps(a.m128, b.m128, c.m128)};
 }
 
-inline Vector3 nmadd(const Vector3& a, const Vector3& b, const Vector3& c)
+inline Vector3 nmadd(const Vector3& a, const Vector3& b, const Vector3& c) noexcept
 {
-    return _mm_fnmadd_ps(a.m128, b.m128, c.m128);
+    return Vector3{_mm_fnmadd_ps(a.m128, b.m128, c.m128)};
 }
 
-inline Vector3 nmsub(const Vector3& a, const Vector3& b, const Vector3& c)
+inline Vector3 nmsub(const Vector3& a, const Vector3& b, const Vector3& c) noexcept
 {
-    return _mm_fnmsub_ps(a.m128, b.m128, c.m128);
+    return Vector3{_mm_fnmsub_ps(a.m128, b.m128, c.m128)};
 }
 #else
-inline Vector3 madd(const Vector3& a, const Vector3& b, const Vector3& c)
+inline Vector3 madd(const Vector3& a, const Vector3& b, const Vector3& c) noexcept
 {
     return a * b + c;
 }
 
-inline Vector3 nmadd(const Vector3& a, const Vector3& b, const Vector3& c)
+inline Vector3 nmadd(const Vector3& a, const Vector3& b, const Vector3& c) noexcept
 {
     return -a * b + c;
 }
 
-inline Vector3 nmsub(const Vector3& a, const Vector3& b, const Vector3& c)
+inline Vector3 nmsub(const Vector3& a, const Vector3& b, const Vector3& c) noexcept
 {
     return -a * b - c;
 }
 
-inline Vector3 msub(const Vector3& a, const Vector3& b, const Vector3& c)
+inline Vector3 msub(const Vector3& a, const Vector3& b, const Vector3& c) noexcept
 {
     return a * b - c;
 }
 #endif
 
-inline Vector3 madd(const float a, const Vector3& b, const Vector3& c)
+inline Vector3 madd(const float a, const Vector3& b, const Vector3& c) noexcept
 {
     return madd(Vector3(a), b, c);
 }
 
-inline Vector3 msub(const float a, const Vector3& b, const Vector3& c)
+inline Vector3 msub(const float a, const Vector3& b, const Vector3& c) noexcept
 {
     return msub(Vector3(a), b, c);
 }
 
-inline Vector3 nmadd(const float a, const Vector3& b, const Vector3& c)
+inline Vector3 nmadd(const float a, const Vector3& b, const Vector3& c) noexcept
 {
     return nmadd(Vector3(a), b, c);
 }
 
-inline Vector3 nmsub(const float a, const Vector3& b, const Vector3& c)
+inline Vector3 nmsub(const float a, const Vector3& b, const Vector3& c) noexcept
 {
     return nmsub(Vector3(a), b, c);
 }
 
 // Based on Matt Pharr's DifferenceOfProducts:
 // https://pharr.org/matt/blog/2019/11/03/difference-of-floats
-inline Vector3 difference_of_products(const Vector3& a, const Vector3& b, const Vector3& c, const Vector3& d)
+inline Vector3 difference_of_products(const Vector3& a, const Vector3& b, const Vector3& c, const Vector3& d) noexcept
 {
     const auto cd  = c * d;
     const auto err = nmadd(c, d, cd);
@@ -407,32 +409,32 @@ inline Vector3 difference_of_products(const Vector3& a, const Vector3& b, const 
 /// Assignment Operators
 ////////////////////////////////////////////////////////////////////////////////
 
-inline Vector3& operator+=(Vector3& a, const Vector3& b)
+inline Vector3& operator+=(Vector3& a, const Vector3& b) noexcept
 {
     return a = a + b;
 }
 
-inline Vector3& operator-=(Vector3& a, const Vector3& b)
+inline Vector3& operator-=(Vector3& a, const Vector3& b) noexcept
 {
     return a = a - b;
 }
 
-inline Vector3& operator*=(Vector3& a, const Vector3& b)
+inline Vector3& operator*=(Vector3& a, const Vector3& b) noexcept
 {
     return a = a * b;
 }
 
-inline Vector3& operator*=(Vector3& a, const float b)
+inline Vector3& operator*=(Vector3& a, const float b) noexcept
 {
     return a = a * b;
 }
 
-inline Vector3& operator/=(Vector3& a, const Vector3& b)
+inline Vector3& operator/=(Vector3& a, const Vector3& b) noexcept
 {
     return a = a / b;
 }
 
-inline Vector3& operator/=(Vector3& a, const float b)
+inline Vector3& operator/=(Vector3& a, const float b) noexcept
 {
     return a = a / b;
 }
@@ -441,12 +443,12 @@ inline Vector3& operator/=(Vector3& a, const float b)
 /// Comparison Operators
 ////////////////////////////////////////////////////////////////////////////////
 
-inline bool operator==(const Vector3& a, const Vector3& b)
+inline bool operator==(const Vector3& a, const Vector3& b) noexcept
 {
     return (_mm_movemask_ps(_mm_cmpeq_ps(a.m128, b.m128)) & 7) == 7;
 }
 
-inline bool operator!=(const Vector3& a, const Vector3& b)
+inline bool operator!=(const Vector3& a, const Vector3& b) noexcept
 {
     return (_mm_movemask_ps(_mm_cmpneq_ps(a.m128, b.m128)) & 7) != 0;
 }
@@ -456,7 +458,7 @@ inline bool operator!=(const Vector3& a, const Vector3& b)
 ////////////////////////////////////////////////////////////////////////////////
 
 #if defined(__SSE4_1__)
-inline float dot(const Vector3& a, const Vector3& b)
+inline float dot(const Vector3& a, const Vector3& b) noexcept
 {
     return _mm_cvtss_f32(_mm_dp_ps(a.m128, b.m128, 0x7F));
 }
@@ -464,7 +466,7 @@ inline float dot(const Vector3& a, const Vector3& b)
 #error Add reduce add
 #endif
 
-inline Vector3 cross(const Vector3& a, const Vector3& b)
+inline Vector3 cross(const Vector3& a, const Vector3& b) noexcept
 {
     return difference_of_products(shuffle<1, 2, 0, 3>(a),
                                   shuffle<2, 0, 1, 3>(b),
@@ -472,47 +474,47 @@ inline Vector3 cross(const Vector3& a, const Vector3& b)
                                   shuffle<1, 2, 0, 3>(b));
 }
 
-inline float sqr_length(const Vector3& a)
+inline float sqr_length(const Vector3& a) noexcept
 {
     return dot(a, a);
 }
 
-inline float rcp_length(const Vector3& a)
+inline float rcp_length(const Vector3& a) noexcept
 {
     return rsqrt(dot(a, a));
 }
 
-inline float rcp_length2(const Vector3& a)
+inline float rcp_length2(const Vector3& a) noexcept
 {
     return rcp(dot(a, a));
 }
 
-inline float length(const Vector3& a)
+inline float length(const Vector3& a) noexcept
 {
     return std::sqrt(dot(a, a));
 }
 
-inline Vector3 normalize(const Vector3& a)
+inline Vector3 normalize(const Vector3& a) noexcept
 {
     return a * rsqrt(dot(a, a));
 }
 
-inline float distance(const Vector3& a, const Vector3& b)
+inline float distance(const Vector3& a, const Vector3& b) noexcept
 {
     return length(a - b);
 }
 
-inline float halfArea(const Vector3& d)
+inline float halfArea(const Vector3& d) noexcept
 {
     return madd(d.x, (d.y + d.z), d.y * d.z);
 }
 
-inline float area(const Vector3& d)
+inline float area(const Vector3& d) noexcept
 {
     return 2.0f * halfArea(d);
 }
 
-inline Vector3 normalize_safe(const Vector3& a)
+inline Vector3 normalize_safe(const Vector3& a) noexcept
 {
     const float d = dot(a, a);
     if (d == 0.0f) [[unlikely]]
@@ -522,19 +524,19 @@ inline Vector3 normalize_safe(const Vector3& a)
 }
 
 /*! differentiated normalization */
-inline Vector3 dnormalize(const Vector3& p, const Vector3& dp)
+inline Vector3 dnormalize(const Vector3& p, const Vector3& dp) noexcept
 {
     const float pp  = dot(p, p);
     const float pdp = dot(p, dp);
     return (pp * dp - pdp * p) * rcp(pp) * rsqrt(pp);
 }
 
-inline Vector3 lerp(const Vector3& v0, const Vector3& v1, const float t)
+inline Vector3 lerp(const Vector3& v0, const Vector3& v1, const float t) noexcept
 {
     return madd(1.0f - t, v0, t * v1);
 }
 
-inline int max_dim(const Vector3& a)
+inline int max_dim(const Vector3& a) noexcept
 {
     const Vector3 b = abs(a);
     if (b.x > b.y) {
@@ -554,11 +556,9 @@ inline int max_dim(const Vector3& a)
 /// Output Operators
 ////////////////////////////////////////////////////////////////////////////////
 
-inline std::ostream& operator<<(std::ostream& cout, const Vector3& a)
+inline std::ostream& operator<<(std::ostream& outs, const Vector3& a)
 {
-    return cout << "(" << a.x << ", " << a.y << ", " << a.z << ")";
+    return outs << "(" << a.x << ", " << a.y << ", " << a.z << ")";
 }
-
-typedef Vector3 Vec3fa_t;
 
 } // namespace sp

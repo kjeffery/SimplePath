f926c09d74616ce283c1d371f98670992982318b
Keith Jeffery <krjeffery@gmail.com>
2022-09-17 15:49:18 -0700

    Add Intersection.h

---
+++ shapes/Intersection.h
@@ -0,0 +1,14 @@
+
+#pragma once
+
+/// @author Keith Jeffery
+
+#include "../math/Vector3.h"
+
+namespace sp {
+struct Intersection
+{
+    Normal3 normal;
+    Point3  point;
+};
+} // namespace sp

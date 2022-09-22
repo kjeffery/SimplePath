83f5c6dfa26e33e2a17493441f5719796864e6a7
Keith Jeffery <krjeffery@gmail.com>
2022-09-21 22:11:44 -0700

    Change to list accelerator

--- base/Scene.h
+++ base/Scene.h
@@ -4,6 +4,7 @@
 ///@author Keith Jeffery
 
 #include "not_null.h"
+#include "SmartToRawPointerIterator.h"
 
 #include "../shapes/Aggregate.h"
 #include "../shapes/ListAccelerator.h"
@@ -15,34 +16,47 @@ namespace sp {
 class Scene
 {
 public:
+    using HitableContainer = std::vector<not_null<std::unique_ptr<const Hitable>>>;
+
+#if 0
     Scene(std::unique_ptr<Aggregate> accelerator_geometry, std::unique_ptr<Aggregate> accelerator_lights)
     : m_accelerator_geometry(std::move(accelerator_geometry))
     , m_accelerator_lights(std::move(accelerator_lights))
     {
     }
+#endif
+
+    explicit Scene(HitableContainer hitables, HitableContainer lights)
+    : m_hitables(std::move(hitables))
+    , m_lights(std::move(lights))
+    , m_accelerator_geometry(SmartToRawPointerIterator{ m_hitables.cbegin() },
+                             SmartToRawPointerIterator{ m_hitables.cend() })
+    , m_accelerator_lights(SmartToRawPointerIterator{ m_lights.cbegin() },
+                           SmartToRawPointerIterator{ m_lights.cend() })
+    {
+    }
 
     bool intersect(const Ray& ray, RayLimits& limits, LightIntersection& isect) const noexcept
     {
-        return m_accelerator_lights->intersect(ray, limits, isect);
+        return m_accelerator_lights.intersect(ray, limits, isect);
     }
 
     bool intersect(const Ray& ray, RayLimits& limits, Intersection& isect) const noexcept
     {
-        return m_accelerator_geometry->intersect(ray, limits, isect);
+        return m_accelerator_geometry.intersect(ray, limits, isect);
     }
 
     bool intersect_p(const Ray& ray, const RayLimits& limits) const noexcept
     {
-        return m_accelerator_geometry->intersect_p(ray, limits);
+        return m_accelerator_geometry.intersect_p(ray, limits);
     }
 
 private:
-    using HitableContainer = std::vector<not_null<std::unique_ptr<Hitable>>>;
-
     // Scene owns the geometry + other hitables.
     HitableContainer m_hitables;
+    HitableContainer m_lights;
 
-    not_null<std::unique_ptr<Aggregate>> m_accelerator_geometry;
-    not_null<std::unique_ptr<Aggregate>> m_accelerator_lights;
+    ListAccelerator m_accelerator_geometry;
+    ListAccelerator m_accelerator_lights;
 };
 } // namespace sp

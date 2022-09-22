54aac3225c35dfe2747d0293e291d7967dd84dd2
Keith Jeffery <krjeffery@gmail.com>
2022-09-21 22:10:12 -0700

    Add better pointer type discovery and make movable

--- base/not_null.h
+++ base/not_null.h
@@ -9,18 +9,41 @@
 // Based off of Microsoft's GSL not_null
 
 namespace sp {
+
+template< class, class = void >
+struct has_pointer_member: std::false_type { };
+
+// specialization recognizes types that do have a nested ::type member:
+template< class T >
+struct has_pointer_member<T, std::void_t<typename T::pointer>> : std::true_type { };
+
+template <typename T, typename Enable = void>
+struct PointerType
+{
+    using pointer = T*;
+};
+
+template <typename T>
+struct PointerType<T, typename std::enable_if<has_pointer_member<T>::value>::type>
+{
+    using pointer = typename T::pointer;
+};
+
 template <typename T>
 concept is_comparable_to_nullptr = std::is_convertible_v<decltype(std::declval<T>() != nullptr), bool>;
 
 template <typename T>
 // clang-format off
-requires is_comparable_to_nullptr<T> && ! std::is_same_v<std::nullptr_t, T>
+requires is_comparable_to_nullptr<T> && (!std::is_same_v<std::nullptr_t, T>)
 class not_null
 // clang-format on
 {
 public:
+    using pointer      = typename PointerType<T>::pointer;
+    using element_type = T;
+
     template <typename U>
-    requires std::is_convertible_v<U, T>
+    requires std::convertible_to<U, T>
     constexpr not_null(U&& u)
     : m_ptr(std::forward<U>(u))
     {
@@ -43,6 +66,8 @@ public:
 
     not_null(const not_null&)            = default;
     not_null& operator=(const not_null&) = default;
+    not_null(not_null&&)                 = default;
+    not_null& operator=(not_null&&)      = default;
 
     constexpr std::conditional_t<std::is_copy_constructible_v<T>, T, const T&> get() const
     {

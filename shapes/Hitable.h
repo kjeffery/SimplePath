53e94676a311f991de68093be45321e2227d4db9
Keith Jeffery <krjeffery@gmail.com>
2022-09-21 22:08:45 -0700

    Add top-level dispatch functions (NVI)

--- shapes/Hitable.h
+++ shapes/Hitable.h
@@ -14,10 +14,25 @@ class Hitable
 public:
     virtual ~Hitable() = default;
 
-    bool  intersect(const Ray& ray, RayLimits& limits, LightIntersection& isect) const noexcept;
-    bool  intersect(const Ray& ray, RayLimits& limits, Intersection& isect) const noexcept;
-    bool  intersect_p(const Ray& ray, const RayLimits& limits) const noexcept;
-    BBox3 get_world_bounds() const noexcept;
+    [[nodiscard]] bool intersect(const Ray& ray, RayLimits& limits, LightIntersection& isect) const noexcept
+    {
+        return intersect_impl(ray, limits, isect);
+    }
+
+    [[nodiscard]] bool intersect(const Ray& ray, RayLimits& limits, Intersection& isect) const noexcept
+    {
+        return intersect_impl(ray, limits, isect);
+    }
+
+    [[nodiscard]] bool intersect_p(const Ray& ray, const RayLimits& limits) const noexcept
+    {
+        return intersect_p_impl(ray, limits);
+    }
+
+    [[nodiscard]] BBox3 get_world_bounds() const noexcept
+    {
+        return get_world_bounds_impl();
+    }
 
 private:
     virtual bool  intersect_impl(const Ray& ray, RayLimits& limits, LightIntersection& isect) const noexcept = 0;

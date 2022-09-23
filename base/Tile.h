e3d42cb459f93b2a88de2307d020abedb729bd2e
Keith Jeffery <krjeffery@gmail.com>
2022-09-23 14:39:04 -0700

    Fix some iterator issues (you can say we're iterating on the design)

--- base/Tile.h
+++ base/Tile.h
@@ -12,7 +12,7 @@ constexpr int k_tile_dimension = 8;
 
 static_assert(is_power_of_two(k_tile_dimension), "We assume power of two for Morton traversal");
 
-//using Tile = BBox2i;
+// using Tile = BBox2i;
 
 class Tile : private BBox2i
 {
@@ -21,13 +21,13 @@ public:
     using BBox2i::get_upper;
 
     explicit Tile(Point2i p) noexcept
-    : BBox2i{p, p + Point2i{k_tile_dimension}}
+    : BBox2i{ p, p + Point2i{ k_tile_dimension } }
     {
     }
 
     friend inline Tile intersect(const BBox2i& a, const Tile& b) noexcept
     {
-        return Tile{intersect(a, static_cast<BBox2i>(b))};
+        return Tile{ intersect(a, static_cast<BBox2i>(b)) };
     }
 
     friend inline Tile intersect(const Tile& a, const BBox2i& b) noexcept
@@ -36,6 +36,10 @@ public:
     }
 
     friend bool operator==(const Tile& a, const Tile& b) noexcept = default;
+    friend bool contains(const Tile& tile, const Point2i& p) noexcept
+    {
+        return contains_open(static_cast<BBox2i>(tile), p);
+    }
 
 private:
     explicit Tile(BBox2i bounds) noexcept
@@ -51,12 +55,17 @@ public:
     using iterator_category = std::random_access_iterator_tag;
     using value_type        = Point2i;
     using difference_type   = int;
+    using reference         = value_type&;
 
-    TilePixelIterator() = default;
+    TilePixelIterator() noexcept
+    : m_index{ 0 }
+    , m_tile{Point2i{0, 0}}
+    {
+    }
 
     explicit TilePixelIterator(Tile tile, int index = 0) noexcept
-    : m_index(index)
-    , m_tile(tile)
+    : m_index{ index }
+    , m_tile{ tile }
     {
     }
 
@@ -105,7 +114,7 @@ public:
         return *this;
     }
 
-    value_type operator[](difference_type n) noexcept
+    value_type operator[](difference_type n) const noexcept
     {
         return dereference(m_index + n);
     }
@@ -117,12 +126,17 @@ public:
     }
 
     friend bool operator==(const TilePixelIterator&, const TilePixelIterator&) noexcept = default;
+    friend std::strong_ordering operator<=>(const TilePixelIterator& a, const TilePixelIterator& b) noexcept
+    {
+        assert(a.m_tile == b.m_tile);
+        return a.m_index <=> b.m_index;
+    }
 
 private:
     value_type dereference(int index) const noexcept
     {
         const auto [x, y] = morton_decode(index);
-        return m_tile.get_lower() + Point2i{x, y};
+        return m_tile.get_lower() + Point2i{ x, y };
     }
 
     int  m_index;
@@ -156,4 +170,13 @@ TilePixelIterator end(Tile tile) noexcept
     return TilePixelIterator(tile, k_tile_dimension * k_tile_dimension);
 }
 
+TilePixelIterator cbegin(Tile tile) noexcept
+{
+    return TilePixelIterator(tile, 0);
+}
+
+TilePixelIterator cend(Tile tile) noexcept
+{
+    return TilePixelIterator(tile, k_tile_dimension * k_tile_dimension);
+}
 } // namespace sp

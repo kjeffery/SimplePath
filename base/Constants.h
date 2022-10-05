7fe9b6f097a4c3d4cab376714d6de40a4574146d
Keith Jeffery <krjeffery@gmail.com>
2022-10-05 16:56:56 -0700

    Add constant for infinity

--- base/Constants.h
+++ base/Constants.h
@@ -11,4 +11,6 @@ struct NoInitType
 constexpr NoInitType no_init{};
 
 constexpr float k_max_less_than_one = 0x1.fffffe0000000p-1f;
+constexpr float k_infinite_distance = std::numeric_limits<float>::max();
+
 } // namespace sp

a3b3283f7ce920fb1b6d5a2f27be307bcc872c8d
Keith Jeffery <krjeffery@gmail.com>
2022-10-16 16:33:58 -0700

    Reduce race condition

--- base/ProgressBar.h
+++ base/ProgressBar.h
@@ -36,12 +36,7 @@ public:
 
         const auto now = std::chrono::system_clock::now();
 
-        if (std::lock_guard<std::mutex> lock(m_mutex); m_step < m_total && now - m_last_time < 1.0s) {
-            return;
-        } else {
-            m_last_time = now;
-        }
-
+        // This work may all be in vain, but we want to limit our locking.
         const auto step = m_step.load(std::memory_order_relaxed);
 
         const unsigned percentage = calc_percentage(step, m_total);
@@ -52,15 +47,19 @@ public:
 
         const std::string bar(numDraw, marker);
 
-        // Sadly, my version of GCC does not yet support syncstream.
-        std::lock_guard<std::mutex> lock(m_mutex);
-        // clang-format off
-        std::cout << '\r'
-                  << std::right << std::setfill(' ') << std::setw(4) << percentage << "% |"
-                  << std::left  << std::setfill(' ') << std::setw(width) << bar << "> "
-                  << step << '/' << m_total
-                  << std::flush;
-        // clang-format on
+        // We force an update at 100% whether or not our time has elapsed.
+        if (std::lock_guard<std::mutex> lock(m_mutex); m_step < m_total && now - m_last_time < 1.0s) {
+            return;
+        } else {
+            m_last_time = now;
+            // clang-format off
+            std::cout << '\r'
+                      << std::right << std::setfill(' ') << std::setw(4) << percentage << "% |"
+                      << std::left  << std::setfill(' ') << std::setw(width) << bar << "> "
+                      << step << '/' << m_total
+                      << std::flush;
+            // clang-format on
+        }
     }
 
 private:

195f188c82b148544924eeb8672285a1090ebb80
Keith Jeffery <krjeffery@gmail.com>
2022-10-20 19:42:36 -0700

    Attempt at smarter Russian roulette

--- Integrators/Integrator.h
+++ Integrators/Integrator.h
@@ -2,6 +2,7 @@
 
 /// @author Keith Jeffery
 
+#include "../base/RunningStats.h"
 #include "../base/Scene.h"
 #include "../math/HSV.h"
 #include "../math/ONB.h"
@@ -19,13 +20,14 @@ class Integrator
 public:
     virtual ~Integrator() = default;
 
-    [[nodiscard]] RGB integrate(const Ray& ray, const Scene& scene, Sampler& sampler) const
+    [[nodiscard]] RGB integrate(const Ray& ray, const Scene& scene, Sampler& sampler, const Point2& pixel_coords) const
     {
-        return integrate_impl(ray, scene, sampler);
+        return integrate_impl(ray, scene, sampler, pixel_coords);
     }
 
 private:
-    virtual RGB integrate_impl(const Ray& ray, const Scene& scene, Sampler& sampler) const = 0;
+    virtual RGB
+    integrate_impl(const Ray& ray, const Scene& scene, Sampler& sampler, const Point2& pixel_coords) const = 0;
 };
 
 // A simple test integrator that ignores the ray direction and simply uses the x and y values.
@@ -39,7 +41,7 @@ public:
     }
 
 private:
-    RGB integrate_impl(const Ray& ray, const Scene&, Sampler&) const override
+    RGB integrate_impl(const Ray& ray, const Scene&, Sampler&, const Point2& pixel_coords) const override
     {
         constexpr float x0 = -2.0f;
         constexpr float x1 = +1.0f;
@@ -50,8 +52,8 @@ private:
         const float dy = (y1 - y0) / m_image_height;
 
         // TODO: intersect with plane in front of camera (transform to local space?)
-        const float& px = ray.get_origin().x;
-        const float& py = ray.get_origin().y;
+        const float& px = pixel_coords.x;
+        const float& py = pixel_coords.y;
 
         const float x = x0 + px * dx;
         const float y = y0 + py * dy;
@@ -92,7 +94,7 @@ private:
 class BruteForceIntegrator : public Integrator
 {
 private:
-    RGB integrate_impl(const Ray& ray, const Scene& scene, Sampler& sampler) const override
+    RGB integrate_impl(const Ray& ray, const Scene& scene, Sampler& sampler, const Point2&) const override
     {
         return do_integrate(ray, scene, sampler, 0);
     }
@@ -134,7 +136,7 @@ private:
 class BruteForceIntegratorIterative : public Integrator
 {
 private:
-    RGB integrate_impl(const Ray& ray, const Scene& scene, Sampler& sampler) const override
+    RGB integrate_impl(const Ray& ray, const Scene& scene, Sampler& sampler, const Point2&) const override
     {
         return do_integrate(ray, scene, sampler);
     }
@@ -142,7 +144,7 @@ private:
     RGB do_integrate(Ray ray, const Scene& scene, Sampler& sampler) const
     {
         RGB throughput = RGB::white();
-        RGB L = RGB::black();
+        RGB L          = RGB::black();
 
         for (int depth = 0; depth < scene.max_depth; ++depth) {
             RayLimits         limits;
@@ -180,7 +182,7 @@ private:
 class BruteForceIntegratorIterativeRR : public Integrator
 {
 private:
-    RGB integrate_impl(const Ray& ray, const Scene& scene, Sampler& sampler) const override
+    RGB integrate_impl(const Ray& ray, const Scene& scene, Sampler& sampler, const Point2&) const override
     {
         return do_integrate(ray, scene, sampler);
     }
@@ -188,8 +190,9 @@ private:
     RGB do_integrate(Ray ray, const Scene& scene, Sampler& sampler) const
     {
         RGB throughput = RGB::white();
-        RGB L = RGB::black();
+        RGB L          = RGB::black();
 
+        constexpr float rr_throughput_luminance_cutoff = 0.1f;
         for (int depth = 0; depth < scene.max_depth; ++depth) {
             RayLimits         limits;
             LightIntersection light_intersection;
@@ -213,13 +216,11 @@ private:
                 const RGB contribution = cosine * shading_result.color / shading_result.pdf;
                 throughput *= contribution;
 
-                // TODO: dynamic
                 if (depth >= scene.min_depth) {
-                    constexpr float mean_throughput_luminance = 0.1f;
                     const float lum = relative_luminance(throughput);
-                    if (lum < mean_throughput_luminance) {
+                    if (lum < rr_throughput_luminance_cutoff) {
                         // q is the probability of continuing
-                        const float q = std::max(0.05f, lum/mean_throughput_luminance);
+                        const float q = std::max(0.05f, lum / rr_throughput_luminance_cutoff);
                         assert(q < 1.0f);
                         if (sampler.get_next_1D() < q) {
                             throughput /= q;
@@ -228,7 +229,6 @@ private:
                         }
                     }
                 }
-
             } else if (hit_light) {
                 L += throughput * light_intersection.L;
                 break;
@@ -238,6 +238,99 @@ private:
         }
         return L;
     }
+};
+
+class BruteForceIntegratorIterativeDynamicRR : public Integrator
+{
+    using StatsType           = RunningStats<float>;
+    using Stats2D             = Array2D<StatsType>;
+    using StatsDepthContainer = std::vector<Stats2D>;
+
+public:
+    BruteForceIntegratorIterativeDynamicRR(int min_depth, int max_depth, int image_width, int image_height)
+    : m_stats(max_depth - min_depth, Stats2D(image_width, image_height))
+    {
+        for (int depth = min_depth; depth < max_depth; ++depth) {
+            for (int y = 0; y < image_height; ++y) {
+                for (int x = 0; x < image_width; ++x) {
+                    assert(get_stats_for_depth(min_depth, depth)(x, y).size() == 0);
+                }
+            }
+        }
+    }
+
+private:
+    RGB integrate_impl(const Ray& ray, const Scene& scene, Sampler& sampler, const Point2& pixel_coords) const override
+    {
+        return do_integrate(ray, scene, sampler, pixel_coords);
+    }
+
+    RGB do_integrate(Ray ray, const Scene& scene, Sampler& sampler, const Point2& pixel_coords) const
+    {
+        RGB throughput = RGB::white();
+        RGB L          = RGB::black();
+
+        constexpr int rr_min_samples = 16;
+        for (int depth = 0; depth < scene.max_depth; ++depth) {
+            RayLimits         limits;
+            LightIntersection light_intersection;
+            const bool        hit_light = scene.intersect(ray, limits, light_intersection);
+            if (Intersection geometry_intersection; scene.intersect(ray, limits, geometry_intersection)) {
+                const Vector3  wo = -ray.get_direction();
+                const Normal3& n  = geometry_intersection.m_normal;
+
+                const auto shading_result = geometry_intersection.m_material->sample(wo, n, sampler);
+                if (shading_result.pdf == 0.0f || shading_result.color == RGB::black()) {
+                    break;
+                }
+
+                // Get next direction
+                const auto& wi                = shading_result.direction;
+                const auto  cosine            = dot(wi, n);
+                const auto  outgoing_position = ray(limits.m_t_max);
+                const Ray   outgoing_ray{ outgoing_position, wi };
+                ray = outgoing_ray;
+
+                const RGB contribution = cosine * shading_result.color / shading_result.pdf;
+                throughput *= contribution;
+
+                if (depth >= scene.min_depth) {
+                    const int pixel_x = static_cast<int>(pixel_coords.x);
+                    const int pixel_y = static_cast<int>(pixel_coords.y);
+                    auto&     stats   = get_stats_for_depth(scene.min_depth, depth)(pixel_x, pixel_y);
+                    if (stats.size() >= rr_min_samples) {
+                        const float mean_throughput_luminance = stats.mean();
+                        const float lum                       = relative_luminance(throughput);
+                        if (lum < mean_throughput_luminance) {
+                            // q is the probability of continuing
+                            const float q = std::max(0.05f, lum / mean_throughput_luminance);
+                            assert(q < 1.0f);
+                            if (sampler.get_next_1D() < q) {
+                                throughput /= q;
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+                    stats.push(relative_luminance(throughput));
+                }
+            } else if (hit_light) {
+                L += throughput * light_intersection.L;
+                break;
+            } else {
+                break;
+            }
+        }
+        return L;
+    }
+
+    Stats2D& get_stats_for_depth(int min_depth, int current_depth) const
+    {
+        assert(current_depth >= min_depth);
+        return m_stats.at(current_depth - min_depth);
+    }
+
+    mutable StatsDepthContainer m_stats;
 };
 
 } // namespace sp

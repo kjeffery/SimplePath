24c1447d6c2ebc55982fefca887e3cc95d3bd644
Keith Jeffery <krjeffery@gmail.com>
2022-11-20 16:23:40 -0800

    Add DEBUG_MODE macro

--- base/Constants.h
+++ base/Constants.h
@@ -13,4 +13,6 @@ constexpr NoInitType no_init{};
 constexpr float k_max_less_than_one = 0x1.fffffe0000000p-1f;
 constexpr float k_infinite_distance = std::numeric_limits<float>::max();
 
+#define DEBUG_MODE !defined(NDEBUG)
+
 } // namespace sp
